23e97e1e11a26650c3ffd925d8c97fa13c0d22d3
jimmy-potato <118575222+jimmy-potato@users.noreply.github.com>
2025-05-08 11:28:25 +0900

    added templates

---
+++ html/baselayout/layout.js
@@ -0,0 +1,24 @@
+async function loadLayoutParts() {
+    //looks for components with class fromLayout
+    const headerEl = document.querySelector('header.fromLayout');
+    const asideEl = document.querySelector('aside.fromLayout');
+    const footerEl = document.querySelector('footer.fromLayout');
+
+    //gets gets base layout files
+    try {
+        const [header, aside, footer] = await Promise.all([
+            fetch('/html/baselayout/header.html').then(res => res.text()),
+            fetch('/html/baselayout/sidebar.html').then(res => res.text()),
+            fetch('/html/baselayout/footer.html').then(res => res.text()),
+        ]);
+
+        //sets previous components using base layout files
+        if (headerEl) headerEl.innerHTML = header;
+        if (asideEl) asideEl.innerHTML = aside;
+        if (footerEl) footerEl.innerHTML = footer;
+    } catch (err) {
+        console.error('Error loading layout parts:', err);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', loadLayoutParts);
